297c5243ef9757c1795bca5313670d7e57385478
IzzArk <faiz.arkan1061@gmail.com>
2025-08-03 18:02:22 +0700

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
-
-
+  base: '/', // Tetap '/' untuk root domain (misalnya yourname.vercel.app)
+  assetsInclude: ['**/*.glb'], // jika kamu menggunakan file 3D
+});
